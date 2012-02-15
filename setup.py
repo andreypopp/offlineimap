887a24feca590cee07cca5ec67115075f288ebd6
Andrey Popp <8mayday@gmail.com>
2012-02-15 23:59:33 +0400

    use setuptools

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 import os
-from distutils.core import setup, Command
+from setuptools import setup
+from distutils.core import Command
 import offlineimap
 import logging
 from test.OLItest import TextTestRunner, TestLoader, OLITestLib
